4a26931d7bef6207498b35461778832ef39cb268
alex-yellow <jeltovas91@gmail.com>
2025-11-04 13:38:37 +0300

    Day 1: Initial Spring Boot project

---
+++ src/main/java/com/example/springlearningjourney/SpringLearningJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlearningjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningJourneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearningJourneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springlearningjourney/SpringLearningJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlearningjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningJourneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
